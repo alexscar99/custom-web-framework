79aff6aca2e31d51c766529d9b6a7e4806b1c0d3
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-11 15:26:40 -0400

    Added methods to 'User' class to delegate responsibilities with altering data to either 'Attributes', 'Eventing', or 'Sync' classes using composition
    'set' method will change the data property on attributes and then trigger a 'change' event
    
    'fetch' will make a request to persisted data based off of id and then call 'set' method with the response
    
    'save' method will get all of the attrs and make a request to save them and after promise resolves it will trigger a 'save' event

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
@@ -29,5 +30,37 @@ export class User {
 
     get get() {
         return this.attributes.get;
+    }
+
+    set(update: UserProps): void {
+        this.attributes.set(update);
+        this.events.trigger('change');
+    }
+
+    fetch(): void {
+        const id = this.get('id');
+
+        if (typeof id !== 'number') {
+            throw new Error('Cannot fetch without an id');
+        }
+
+        this.sync.fetch(id).then(
+            (res: AxiosResponse): void => {
+                this.set(res.data);
+            }
+        );
+    }
+
+    save(): void {
+        this.sync
+            .save(this.attributes.getAll())
+            .then(
+                (res: AxiosResponse): void => {
+                    this.trigger('save');
+                }
+            )
+            .catch(() => {
+                this.trigger('error');
+            });
     }
 }
